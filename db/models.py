37f09cdd1121184031a76d61d52366ed83001429
Allfabread <148641940+Allfabread@users.noreply.github.com>
2024-05-21 18:28:09 +0300

    Update models.py

--- db/models.py
+++ db/models.py
@@ -17,52 +17,52 @@ class User(Base):
     balance = Column(Float, default=0)
 
     
-class Category(Base):
-    __tablename__ = 'categories'
+# class Category(Base):
+#     __tablename__ = 'categories'
     
-    id = Column(Integer, primary_key=True)
-    name = Column(String(30))
-    icon = Column(String(70))
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(30))
+#     icon = Column(String(70))
 
 
-class FreelancersCategory(Base):
-    __tablename__ = 'categories_of_freelancers'
+# class FreelancersCategory(Base):
+#     __tablename__ = 'categories_of_freelancers'
     
-    id = Column(Integer, primary_key=True)
-    freelancer_id = Column(Integer, ForeignKey(f'{User.__tablename__}.id'))
-    category_id = Column(Integer, ForeignKey(f'{Category.__tablename__}.id'))
+#     id = Column(Integer, primary_key=True)
+#     freelancer_id = Column(Integer, ForeignKey(f'{User.__tablename__}.id'))
+#     category_id = Column(Integer, ForeignKey(f'{Category.__tablename__}.id'))
 
 
-class Task(Base):
-    __tablename__ = 'tasks'
+# class Task(Base):
+#     __tablename__ = 'tasks'
     
-    id = Column(Integer, primary_key=True)
-    customer_id = Column(Integer, ForeignKey(f'{User.__tablename__}.id'))
-    category_id = Column(Integer, ForeignKey(f'{Category.__tablename__}.id'))
-    title = Column(String(50))
-    description = Column(String(500))
-    created = Column(DateTime, default=datetime.now)
+#     id = Column(Integer, primary_key=True)
+#     customer_id = Column(Integer, ForeignKey(f'{User.__tablename__}.id'))
+#     category_id = Column(Integer, ForeignKey(f'{Category.__tablename__}.id'))
+#     title = Column(String(50))
+#     description = Column(String(500))
+#     created = Column(DateTime, default=datetime.now)
     
 
-class Review(Base):
-    __tablename__ = 'reviews'
+# class Review(Base):
+#     __tablename__ = 'reviews'
     
-    id = Column(Integer, primary_key=True)
-    to_id = Column(Integer, ForeignKey(f'{User.__tablename__}.id'))
-    from_id = Column(Integer, ForeignKey(f'{User.__tablename__}.id'))
-    created = Column(DateTime, default=datetime.now)
-    content = Column(String(150))
-    rating = Column(Integer) # 1 - 5
+#     id = Column(Integer, primary_key=True)
+#     to_id = Column(Integer, ForeignKey(f'{User.__tablename__}.id'))
+#     from_id = Column(Integer, ForeignKey(f'{User.__tablename__}.id'))
+#     created = Column(DateTime, default=datetime.now)
+#     content = Column(String(150))
+#     rating = Column(Integer) # 1 - 5
 
 
-class Message(Base):
-    __tablename__ = 'messages'
+# class Message(Base):
+#     __tablename__ = 'messages'
     
-    id = Column(Integer, primary_key=True)
-    to_id = Column(Integer, ForeignKey(f'{User.__tablename__}.id'))
-    from_id = Column(Integer, ForeignKey(f'{User.__tablename__}.id'))
-    content = Column(String(300))
-    created = Column(DateTime, default=datetime.now)
+#     id = Column(Integer, primary_key=True)
+#     to_id = Column(Integer, ForeignKey(f'{User.__tablename__}.id'))
+#     from_id = Column(Integer, ForeignKey(f'{User.__tablename__}.id'))
+#     content = Column(String(300))
+#     created = Column(DateTime, default=datetime.now)
     
      
 
